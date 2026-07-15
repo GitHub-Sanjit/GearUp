1d01bd6102cc32298de9e69c2a051de2da05939a
sanjit <sanjit.est14@gmail.com>
2026-07-15 19:30:18 +0600

    Auth Validation complete

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { authValidation } from "./auth.validation";
 
 const router = Router();
 
-router.post("/login", authController.loginUser);
+router.post(
+  "/login",
+  validateRequest(authValidation.loginValidationSchema),
+  authController.loginUser,
+);
 
 router.post("/refresh-token", authController.refreshToken);
 
---
+++ src/modules/auth/auth.validation.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+const loginValidationSchema = z.object({
+  body: z.object({
+    email: z
+      .string()
+      .email("Invalid email address"),
+
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters"),
+  }),
+});
+
+export const authValidation = {
+  loginValidationSchema,
+};
